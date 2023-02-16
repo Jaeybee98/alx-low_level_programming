867badfe1af4c71f97d82e748240a31c96771844
Ajibola Junaid <jaeybee98@gmail.com>
2023-02-16 05:52:13 -0800

    Added a new documention(comments) for main.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ *main is the entry point of the program.
+ *return is always (0) for function main.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
