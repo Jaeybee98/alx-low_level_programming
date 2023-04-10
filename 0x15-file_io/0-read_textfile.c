7ea36b73f0a6a66fcc8b0c3a92250b684fcf1e8a
Ajibola Junaid <jaeybee98@gmail.com>
2023-04-10 05:50:53 -0700

    A  function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,50 @@
-#include <stdio.h>
-#include "holberton.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
- * @filename: file.
- * @letters: Number of letters it should read and print.
- * Return: Actual number of letters it could read and print.
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, res_read, res_write;
-	char *buf;
+	int fd; /* file descriptor */
+	ssize_t n_read, n_wrote;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
+
+	/* open */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+
+	/* malloc buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	res_read = read(fd, buf, letters);
-	if (res_read == -1)
+
+	/* read */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
 	{
-		free(buf);
+		free(buffer);
+		close(fd);
 		return (0);
 	}
-	res_write = write(STDOUT_FILENO, buf, res_read);
-	if (res_write == -1 || res_read != res_write)
+
+	/* write */
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (n_wrote == -1)
 	{
-		free(buf);
+		free(buffer);
+		close(fd);
 		return (0);
 	}
-	free(buf);
+
 	close(fd);
-	return (res_write);
+	return (n_read);
+
 }
